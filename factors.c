88e9ae25b10d33f60c8998dea933a2e5634498dd
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-10-04 10:16:34 -0700

    hdgdvdd

--- factors.c
+++ factors.c
@@ -2,40 +2,56 @@
 #include <stdlib.h>
 
 /**
- * Function to find two factors of n.
- * Returns 1 if factors are found and 0 if the number is prime.
+ * factorize - finds two factors of a given number
+ * @n: the number to be factorized
+ * Return: 1 if factors found, 0 if prime
  */
-int factorize(unsigned long long n) {
-    unsigned long long i;
-    for (i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            printf("%llu=%llu*%llu\n", n, n / i, i);
-            return 1;
-        }
-    }
-    // If we reach here, n is prime or no factors found.
-    printf("%llu=%llu*1\n", n, n);
-    return 0;
+int factorize(unsigned long long n)
+{
+	unsigned long long i;
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("%llu=%llu*%llu\n", n, n / i, i);
+			return (1);
+		}
+	}
+	printf("%llu=%llu*1\n", n, n);
+	return (0);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
+/**
+ * main - entry point for the factorization program
+ * @argc: argument count
+ * @argv: argument vector (array of strings)
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	unsigned long long num;
 
-    // Open the file
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        perror("Error opening file");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return (1);
+	}
 
-    unsigned long long num;
-    while (fscanf(file, "%llu", &num) != EOF) {
-        factorize(num);
-    }
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		perror("Error opening file");
+		return (1);
+	}
 
-    fclose(file);
-    return 0;
+	while (fscanf(file, "%llu", &num) != EOF)
+	{
+		factorize(num);
+	}
+
+	fclose(file);
+	return (0);
 }
